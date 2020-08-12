99dce547b196b2e7872deffe0549babb6bb6168c
lmhgithi <lmhfox@foxmail.com>
2020-08-12 19:49:03 +0800

    [liminghao] finish e4 and fix redirect in e3

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch, Redirect} from 'react-router-dom';
 import Home from './Home'
 import Profile from './Profile';
 import About from './About';
 import Product from './Product';
 import Products from './Products';
+import NotMatch from './NotMatch'
 
 class App extends Component {
   render() {
@@ -20,13 +21,15 @@ class App extends Component {
           </div>
           <div className="body">
             <Switch>
-              <Route exact path='/' component={Home} />
+              {/* <Route exact path='/' component={Home} /> */}
               <Route exact path='/my-profile' component={Profile} />
               <Route exact path='/about-us' component={About} />
+              <Redirect from='/goods' to='/products' />
               <Route exact path='/products' component={Products} />
               <Route exact path='/goods' component={Products} />
               <Route exact path='/products/:id' component={Product} />
-              <Route component={Home} />
+              <Route exact path='/' component={Home} />
+              <Route component={NotMatch} />
             </Switch>
           </div>
         </Router>
