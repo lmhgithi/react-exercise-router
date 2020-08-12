a18b14f4e557e44af57b9e1f367d6c31f092ba9a
lmhgithi <lmhfox@foxmail.com>
2020-08-12 20:27:57 +0800

    [liminghao] finish e4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route, Switch } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import User from "./User";
@@ -11,10 +11,13 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
-            <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/notMatch' component={NotMatch} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/:user' component={User} />
+
+          </Switch>
         </Router>
       </div>
     );
--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+class User extends Component {
+  componentDidMount() {
+    if(!parseInt(this.props.match.params.user)){
+      this.props.history.push('/notMatch')
+    }
+  }
+  render(){
+    return (
+      <div>
+        User profile page.
+      </div>
+    );
+  }
 };
+
 
 export default User;
